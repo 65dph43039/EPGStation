69dc9052c214821f26f9394ceb5f53b6090298e1
Le Quang Huy <65dph43039@student.huc.edu.vn>
2025-04-28 00:07:27 +0700

    Update MirakurunClientModel.ts
    change to epgstation api

--- src/model/MirakurunClientModel.ts
+++ src/model/MirakurunClientModel.ts
@@ -1,70 +1,32 @@
 import * as fs from 'fs';
 import { inject, injectable } from 'inversify';
-import mirakurun from 'mirakurun';
 import * as path from 'path';
-import * as url from 'url';
 import IConfigFile from './IConfigFile';
 import IConfiguration from './IConfiguration';
 import IMirakurunClientModel from './IMirakurunClientModel';
 
 /**
- * mirakurun client のインスタンスを生成する
+ * EPGStation client to fetch EPG data
  */
 @injectable()
-export default class MirakurunClientModel implements IMirakurunClientModel {
-    private client: mirakurun;
+export default class EPGStationClientModel implements IMirakurunClientModel {
     private config: IConfigFile;
 
     constructor(@inject('IConfiguration') conf: IConfiguration) {
-        this.client = new mirakurun();
         this.config = conf.getConfig();
-
-        this.setClient();
     }
 
     /**
-     * mirakurun client の設定
+     * Fetch EPG data from EPGStation
+     * @return EPG data
      */
-    private setClient(): void {
-        const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', '..', 'package.json')).toString());
-        const mirakurunPath = this.config.mirakurunPath;
-
-        /**
-         * Copyright (c) 2016 Yuki KAN and Chinachu Project Contributors
-         * Released under the MIT license
-         * http://opensource.org/licenses/mit-license.php
-         */
-        if (/\\\\.\\pipe/.test(mirakurunPath)) {
-            this.client.socketPath = mirakurunPath;
-        } else if (/(?:\/|\+)unix:/.test(mirakurunPath) === true) {
-            const standardFormat = /^http\+unix:\/\/([^\/]+)(\/?.*)$/;
-            const legacyFormat = /^http:\/\/unix:([^:]+):?(.*)$/;
-
-            if (standardFormat.test(mirakurunPath) === true) {
-                this.client.socketPath = mirakurunPath.replace(standardFormat, '$1').replace(/%2F/g, '/');
-                this.client.basePath = path.posix.join(
-                    mirakurunPath.replace(standardFormat, '$2'),
-                    this.client.basePath,
-                );
-            } else {
-                this.client.socketPath = mirakurunPath.replace(legacyFormat, '$1');
-                this.client.basePath = path.posix.join(mirakurunPath.replace(legacyFormat, '$2'), this.client.basePath);
-            }
-        } else {
-            const urlObject = url.parse(mirakurunPath);
-            this.client.host = <string>urlObject.hostname;
-            this.client.port = Number(<string>urlObject.port);
-            this.client.basePath = path.posix.join(<string>urlObject.pathname, <string>this.client.basePath);
+    public async getEPGData(): Promise<any> {
+        const epgPath = this.config.epgPath; // Assume `epgPath` is defined in the configuration
+        if (!epgPath) {
+            throw new Error('EPG Path is not configured');
         }
 
-        this.client.userAgent = `${pkg.name}/${pkg.version}`;
-    }
-
-    /**
-     * mirakurun client を返す
-     * @return mirakurun client
-     */
-    public getClient(): mirakurun {
-        return this.client;
+        const epgData = JSON.parse(fs.readFileSync(path.resolve(epgPath), 'utf-8'));
+        return epgData;
     }
 }
